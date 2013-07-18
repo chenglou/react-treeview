26df75a1e722f3ab926f8e1a43c2c41750c8fa94
Cheng Lou <chenglou92@gmail.com>
2013-07-17 21:36:55 -0400

    parsing demo complete

--- examples/data-source.js
+++ examples/data-source.js
@@ -2,43 +2,66 @@
 * @jsx React.DOM
 */
 
-var treeRoot = <TreeNode>Company people</TreeNode>;
-React.renderComponent(treeRoot, document.body);
+/*
+  either run this file through the jsx transformation tool or include them
+  directly in an html file. More info:
+  http://facebook.github.io/react/docs/getting-started.html
+*/
 
-var data = {
+// server just sent back some data!
+var rawData = {
   'Employees': {
     'Paul Gordon': {
       age: 25,
       sex: 'male',
-      role: 'coder',
+      role: 'coder'
     },
     'Sarah Lee': {
       age: 23,
       sex: 'female',
-      role: 'jqueryer',
+      role: 'jqueryer'
     },
   },
   'CEO': {
     'Drew Anderson': {
       age: 35,
       sex: 'male',
-      role: 'boss',
+      role: 'boss'
     }
   }
 };
 
-for (var category in data) {
-  var categoryNode = treeRoot.append(<TreeNode><b>{category}</b></TreeNode>);
-  for (var person in data[category]) {
-    var personNode = categoryNode.append(<TreeNode>{person}</TreeNode>);
-    for (var info in data[category][person]) {
-      personNode.append(
-        <TreeNode>
-          <i>
-            {info + ': ' + data[category][person][info]}
-          </i>
-        </TreeNode>
-      );
+// time to parse it into a format the tree view recognizes
+// [
+//   {displayNode: bla, children: [bla, bla]},
+//   {displayNode: bla, children: [bla, bla]},
+// ]
+var formattedData = [];
+
+for (var rankName in rawData) {
+  var rankNode = {
+    // simply putting a string here doesn't work. The tree view needs a node
+    displayNode: <b>{rankName}</b>,
+    children: []
+  };
+  for (var personName in rawData[rankName]) {
+    var personNode = {
+      displayNode: <span>{personName}</span>,
+      children: []
+    };
+    for (var personInfo in rawData[rankName][personName]) {
+      var personInfoNode = {
+        displayNode:
+          <span>
+            {personInfo + ': ' + rawData[rankName][personName][personInfo]}
+          </span>
+      };
+      personNode.children.push(personInfoNode);
     }
+    rankNode.children.push(personNode);
   }
+  formattedData.push(rankNode);
 }
+
+var companyPeople = <TreeView source={formattedData}/>;
+React.renderComponent(companyPeople, document.body);
