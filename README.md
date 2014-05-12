10033f4f30140bed40d9d54a9476a635baaeb216
Cheng Lou <chenglou92@gmail.com>
2014-05-12 01:09:41 -0400

    Update example following new version update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-This branch demos the `examples/` folder in master . [Go ahead and play with it!](http://chenglou.github.io/react-treeview/)
+This branch demos the `examples/` folder in master. [Go ahead and play with it!](http://chenglou.github.io/react-treeview/)
+
+If you feel more adventurous: the entire js source code is only ~50 lines of code.
---
+++ bower_components/react-treeview/react-treeview.jsx
@@ -0,0 +1,56 @@
+/** * @jsx React.DOM */
+
+(function (window, React){
+  'use strict';
+
+  var TreeView = React.createClass({
+    propTypes: {
+      collapsed: React.PropTypes.bool,
+      defaultCollapsed: React.PropTypes.bool,
+      nodeLabel: React.PropTypes.renderable.isRequired
+    },
+
+    getInitialState: function() {
+      return {collapsed: this.props.defaultCollapsed};
+    },
+
+    handleClick: function(a, b, c) {
+      this.setState({
+        collapsed: !this.state.collapsed
+      });
+      this.props.handleClick && this.props.handleClick(a, b, c);
+    },
+
+    render: function() {
+      var collapsed = this.props.collapsed != null ?
+        this.props.collapsed :
+        this.state.collapsed;
+
+      var arrowClassName = 'tree-view_arrow';
+      var containerClassName = 'tree-view_children';
+      if (collapsed) {
+        arrowClassName += ' tree-view_arrow-collapsed';
+        containerClassName += ' tree-view_children-collapsed';
+      }
+
+      var arrow =
+        <div className={arrowClassName} onClick={this.handleClick}>▾</div>;
+
+      return (
+        <div className="tree-view">
+          {this.transferPropsTo(arrow)}
+          {this.props.nodeLabel}
+          <div className={containerClassName}>
+            {this.props.children}
+          </div>
+        </div>
+      );
+    }
+  });
+
+  if (typeof module === 'undefined') {
+    window.TreeView = TreeView;
+  } else {
+    module.exports = TreeView;
+  }
+})(window, typeof require === 'function' ? require('React') : React);
