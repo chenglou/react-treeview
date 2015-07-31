bfde3b78cb02c55713b8bd7f792ea01a90478432
Cheng Lou <chenglou92@gmail.com>
2015-07-31 00:21:35 -0700

    Move and refactor react-treeview to use babel
    Sugar + using webpack's (or babel's at dev time) import parsing. No more
    ad-hoc UMD code.

---
+++ src/react-treeview.jsx
@@ -0,0 +1,59 @@
+import React, {PropTypes} from 'react';
+
+const TreeView = React.createClass({
+  propTypes: {
+    collapsed: PropTypes.bool,
+    defaultCollapsed: PropTypes.bool,
+    nodeLabel: PropTypes.node.isRequired,
+  },
+
+  getInitialState() {
+    return {collapsed: this.props.defaultCollapsed};
+  },
+
+  handleClick(...args) {
+    this.setState({collapsed: !this.state.collapsed});
+    if (this.props.onClick) {
+      this.props.onClick(...args);
+    }
+  },
+
+  render() {
+    const {
+      collapsed = this.state.collapsed,
+      className = '',
+      nodeLabel,
+      children,
+      ...rest,
+    } = this.props;
+
+    let arrowClassName = 'tree-view_arrow';
+    let containerClassName = 'tree-view_children';
+    if (collapsed) {
+      arrowClassName += ' tree-view_arrow-collapsed';
+      containerClassName += ' tree-view_children-collapsed';
+    }
+
+    const arrow =
+      <div
+        {...rest}
+        className={className + ' ' + arrowClassName}
+        onClick={this.handleClick}>
+        ▾
+      </div>;
+
+    return (
+      <div className="tree-view">
+        <div className="tree-view_item">
+          {arrow}
+          {nodeLabel}
+        </div>
+        <div className={containerClassName}>
+          {children}
+        </div>
+      </div>
+    );
+  },
+});
+
+export default TreeView;
