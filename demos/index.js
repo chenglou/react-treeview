5720d089fe47e507ee23062c8f2a6f841ba067ce
Cheng Lou <chenglou92@gmail.com>
2016-04-18 00:19:28 -0700

    Fix case sensitivity in demo; bump ver
    Fixes #30, fixes #32

--- demos/index.js
+++ demos/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Controlled from './Controlled';
-import Uncontrolled from './Uncontrolled';
+import Controlled from './controlled';
+import Uncontrolled from './uncontrolled';
 
 ReactDOM.render(<Controlled />, document.getElementById('controlled'));
 ReactDOM.render(<Uncontrolled />, document.getElementById('uncontrolled'));
